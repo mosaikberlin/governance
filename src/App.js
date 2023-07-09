b07aacd49d7723ba359cbacfc806b2b96b308478
Carsten Koch <carsten.b.koch@gmail.com>
2023-07-09 17:25:02 +0200

    fixing visbility of graph by removing div

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import BarChart from "./charts/BarChart";
 
 const App = () => (
-  <div id="org-chart-d3">
+  //<div id="org-chart-d3">
     <BarChart
       data={[12, 5, 6, 6, 9, 10]}
       width={700}
       height={300}/>
-  </div>
+  //</div>
 )
 
 export default App;
