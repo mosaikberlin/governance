88a2d138673cee2d7c201b84d7427d5605756b86
Carsten Koch <carsten.b.koch@gmail.com>
2023-07-09 19:47:08 +0200

    Change on which div d3 is applied

--- src/charts/BarChart.jsx
+++ src/charts/BarChart.jsx
@@ -10,7 +10,7 @@ class BarChart extends Component {
         //const { data, width, height } = this.props;
         const data = [12,5,6,6,9,10];
 
-        const svg = d3.select("body")
+        const svg = d3.select("#org-chart-d3")
             .append("svg")
             .attr("width", 700)
             .attr("height", 300);
