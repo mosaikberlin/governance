b80f9b0dc6c2ee0a4e8006a06a15083903549dd3
Carsten Koch <carsten.b.koch@gmail.com>
2023-07-09 22:22:41 +0200

    making graph dynamic with width and height of the bars

--- src/charts/BarChart.jsx
+++ src/charts/BarChart.jsx
@@ -10,23 +10,28 @@ class BarChart extends Component {
         //const { data, width, height } = this.props;
         const { wrapperId } = this.props;
         const data = [12,5,6,6,9,10];
+        const width = 600;
+        const height = 300;
+        const barWidth = width / data.length;
+        const valueRange = Math.max(data) - Math.min(data, 0);
+        const heightPerPoint = height / valueRange;
 
         //const svg = d3.select("#org-chart-d3")
         const svg = d3.select(wrapperId)
             .append("svg")
-            .attr("width", 700)
-            .attr("height", 300);
-            //.attr("width", width)
-            //.attr("height", height);
+            //.attr("width", 700)
+            //.attr("height", 300);
+            .attr("width", width)
+            .attr("height", height);
 
         svg.selectAll("rect")
             .data(data)
             .enter()
             .append("rect")
-            .attr("x", (d, i) => i * 70)
-            .attr("y", (d, i) => 300 - 10 * d)
-            .attr("width", 65)
-            .attr("height", (d, i) => d * 10)
+            .attr("x", (d, i) => i * barWidth)
+            .attr("y", (d, i) => height - heightPerPoint * d)
+            .attr("width", barWidth * 0.92)
+            .attr("height", (d, i) => d * heightPerPoint)
             .attr("fill", "green");
     }
 
