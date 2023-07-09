eeaaf80ceb832374e4f845ada250334e7887638e
Carsten Koch <carsten.b.koch@gmail.com>
2023-07-09 14:12:49 +0200

    BarChart component finished

--- src/charts/BarChart.jsx
+++ src/charts/BarChart.jsx
@@ -1,5 +1,5 @@
-import React, {Component} from 'react';
-import * as d3 from "d3";
+import React, { Component } from 'react'
+import * as d3 from 'd3'
 
 class BarChart extends Component {
     componentDidMount() {
@@ -8,7 +8,24 @@ class BarChart extends Component {
 
     drawChart() {
         const data = [12, 5, 6, 6, 9, 10];
-        const svg = d3.select("body").append("svg").attr("width", 700).attr("height", 300);
+
+        const svg = d3.select("body")
+            .append("svg")
+            .attr("width", 700)
+            .attr("height", 300);
+
+        svg.selectAll("rect")
+            .data(data)
+            .enter()
+            .append("rect")
+            .attr("x", (d, i) => i * 70)
+            .attr("y", (d, i) => 300 - 10 * d)
+            .attr("width", 65)
+            .attr("height", (d, i) => d * 10)
+            .attr("fill", "green");
+    }
+    render() {
+        return <div id={"#" + this.props.id}></div>
     }
 }
 
